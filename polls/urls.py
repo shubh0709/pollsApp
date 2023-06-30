bd8c64a3306cfacd38fdf266fe11ddc4cca46d58
shubh0709 <singhshubh0709@gmail.com>
2023-06-30 14:45:29 +0530

    fixed bugs + added more tests

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("__debug__/", include("debug_toolbar.urls")),
     path("polls/", include("app_polls.urls")),
     path("admin/", admin.site.urls),
 ]
